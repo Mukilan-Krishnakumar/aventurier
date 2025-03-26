7b5c653a7be17929becb91f5d76dd602ea71f086
Mukilan Krishnakumar <mukilan.git@gmail.com>
2025-03-26 21:44:22 +0530

    Data Model : Vocabulary Module

--- aventurier/aventurier/settings.py
+++ aventurier/aventurier/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 import os
 
 from dotenv import load_dotenv
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- aventurier/core/models.py
+++ aventurier/core/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+
 # Create your models here.
+class BaseModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Language(BaseModel):
+    name = models.TextField()
+
+
+class LanguageFramework(BaseModel):
+    name = models.TextField()
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+
+class Levels(BaseModel):
+    language_framework = models.OneToOneField(
+        LanguageFramework, on_delete=models.CASCADE, primary_key=True
+    )
+    choices = models.JSONField()
+
+
+class PartOfSpeech(BaseModel):
+    name = models.TextField()
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    choices = models.JSONField()
+
+
+class VocabItem(BaseModel):
+    term = models.TextField()
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    definition = models.TextField()
+    examples = models.TextField()
+    # TODO: Modify this to a self-referential field
+    synonyms = models.TextField()
+    part_of_speech = models.ForeignKey(PartOfSpeech, on_delete=models.CASCADE)
